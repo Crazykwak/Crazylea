bf65451607cb137fd63704f256a190b6d78baaec
Crazykwak <kwj1830@naver.com>
2021-10-10 00:40:14 +0900

    fetch practice2

--- onlie Shopping/main.js
+++ onlie Shopping/main.js
@@ -1,11 +1,38 @@
 "use strict"
 
-let test = {};
-
-const comeOnDB = () => {
-    fetch('data/item.json')
+//JSON load
+function comeOnDB() {
+    return fetch('data/data.json')
         .then(Response => Response.json())
-        .then(temp => console.log(temp))
+        .then((json) => json.items)
+        .catch(err => alert('안되겠는데요'))
+};
+
+comeOnDB().then(items => console.log(items))
+
+//display json's items
+function getItem(items) {
+    return `
+    <li class="item">
+        <img src="${items.image}" />
+    </li>
+    <span>${items.gender}, ${items.size}</span>
+    `;
 }
 
-console.log(test);
+function showMeItem(items){
+    const container = document.querySelector(`.items`);
+    container.innerHTML = items.map(items => getItem(items)).join('');
+}
+
+function setEvent(items) {
+    const logo = document.querySelector('.logo');
+    logo.addEventListener('click',() => showMeItem(items));
+}
+
+//실제 동작
+comeOnDB()
+    .then(items => {
+        showMeItem(items);
+        setEvent(items);
+    })
